034b52aac9f0986bfc38e3b0f2851b0023050557
LouellaCreemers <louellacreemers@gmail.com>
2026-06-02 23:15:09 +0200

    Revert "Add Brotli and Gzip compression, enforce HTTPS, and configure security headers in Nitro setup"
    This reverts commit 34e0aff63b1814354c18aa8cb1e7ea21e683fd9c.

--- server/middleware/force-https.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  const forwardedProto = getHeader(event, 'x-forwarded-proto')
-  const host = getHeader(event, 'host')
-
-  if (forwardedProto === 'http' && host) {
-    const location = `https://${host}${event.node.req.url || '/'}`
-    return sendRedirect(event, location, 301)
-  }
-})
